e831caf790daa3d7258b8d0bf045f3ac88ca0712
manucitavm-web <manucitavm@gmail.com>
2026-02-26 13:23:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import streamlit as st
 from PIL import Image
 st.title("Mi primera app")
 st.header("Esta es mi página de presentación")
-image= Imagen.open("Flores.png")
+image= Imagen.open("Flores.webp")
 st.image(image, caption="Esta es mi imagen")
